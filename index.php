27d4adb53525071763b190539309e6538fb8e993
Jos-gs <gonsianjoss@gmail.com>
2025-10-03 15:27:52 -0500

    Update visit counter value, add logo icon, and modify HTML head section

--- index.php
+++ index.php
@@ -20,12 +20,12 @@ file_put_contents($contador_file, $conteo_actual);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>M&D ELECTRIMEC S.A.C</title>
+    
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
-    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="icon" href="logo.ico" ">
 
     <link rel="stylesheet" href="css/style.css">
 </head>
